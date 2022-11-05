b0d050547e19ee7ec249b95f0e9ee58f4416dd1e
mesutilica <mesut.ilica@gmail.com>
2022-11-05 17:35:37 +0300

    5-11-2022 Mvc section dayız

---
+++ AspNetCoreMVCProjesi/Controllers/MVC03PostBackController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVCProjesi.Controllers
+{
+    public class MVC03PostBackController : Controller
+    {
+        [HttpGet] // soldaki attrübütü vermesek de varsayılan değer budur.
+        public IActionResult Index() // Action'ın üstünde herhangi bir metot türü belirtilmezse varsayılan olarak bu metot GET metodudur
+        {
+            return View();
+        }
+        [HttpPost] // Soldaki attribute ü eklediğimzde aşağıdaki action sadece sayfada post işlemi gerçekleştiğinde çalışır.
+        public IActionResult Index(string txtad) // Bu actiton'ın türü HttpPost dur. Bir metodun aynısını yaparsak mvc buna izin vermez. Bunu çözmek için metot imzasında herhangi bir değişken parametresi vermemiz yeterlidir.
+        {
+            return View();
+        }
+    }
+}
