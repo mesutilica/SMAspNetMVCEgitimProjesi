ec435905ca7b27199055291f1ddb68040ca8fc8f
mesutilica <mesut.ilica@gmail.com>
2022-11-06 10:08:13 +0300

    6-11-2022 Partial

---
+++ AspNetCoreMVCProjesi/Controllers/MVC07PartialController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVCProjesi.Controllers
+{
+    public class MVC07PartialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
