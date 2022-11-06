10f430841cf4ed719c91456916b4e97433045099
mesutilica <mesut.ilica@gmail.com>
2022-11-06 11:03:51 +0300

    6-11-2022 controller to view

---
+++ AspNetCoreMVCProjesi/Controllers/MVC08ControllerToViewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVCProjesi.Controllers
+{
+    public class MVC08ControllerToViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            // 3 Temel yöntemle veri gönerilebiliyor
+
+            // 1- ViewBag : Tek kullanımlık ömrü var
+            ViewBag.UrunKategorisi = "Bilgisayar";
+
+            // 2- ViewData : Tek kullanımlık ömrü var
+            ViewData["UrunAdi"] = "Laptop";
+
+            // 3- TempData : 2 kullanımlık ömrü var
+            TempData["UrunFiyati"] = 18000;
+
+            var deneme = "test";
+            
+            return View();
+        }
+    }
+}
