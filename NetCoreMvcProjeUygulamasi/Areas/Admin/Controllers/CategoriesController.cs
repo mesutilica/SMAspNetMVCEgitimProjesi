603398d9dc57c0ca74488fcb27bf73fe0aec8297
mesutilica <mesut.ilica@gmail.com>
2022-12-10 13:03:36 +0300

    10-12-2022 kategori ekleme tamam

---
+++ NetCoreMvcProjeUygulamasi/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCoreMvcProjeUygulamasi.Data;
+using NetCoreMvcProjeUygulamasi.Entities;
+
+namespace NetCoreMvcProjeUygulamasi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CategoriesController
+        public async Task<ActionResult> IndexAsync()
+        {
+            var model = await _context.Categories.ToListAsync();
+            return View(model);
+        }
+
+        // GET: CategoriesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CategoriesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category, IFormFile? Image)
+        {
+            try
+            {
+                if (Image is not null)
+                {
+                    string klasor = Directory.GetCurrentDirectory() + "/wwwroot/Img/" + Image.FileName; // yükleme yapacağımız klasörü belirttik
+                    using var stream = new FileStream(klasor, FileMode.Create); // yükleme için gerekli veri akışı oluşturduk
+                    Image.CopyTo(stream); // veri akışını kullanarak yükleme yaptık
+                    category.Image = Image.FileName;
+                }
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
